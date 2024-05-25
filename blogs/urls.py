c5289585ff49bd74c69af544aa3d29163f4696cf
Jose Miguel <jmvr.2310@gmail.com>
2024-05-25 11:37:44 -0400

    delete post added

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView
+from .views import (BlogListView,
+                    BlogCreateView,
+                    BlogDetailView,
+                    BlogUpdateView,
+                    BlogDeleteView)
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("post/create/", BlogCreateView.as_view(), name="create"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="detail"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="update"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="delete"),
 ]
